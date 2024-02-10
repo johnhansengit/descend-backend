395e460c1ba6bba1474433705c06fc21a334698c
John Hansen <jhb.rgn@gmail.com>
2024-02-10 12:47:31 -0500

    Debug Settings

--- controllers/StatsController.js
+++ controllers/StatsController.js
@@ -2,51 +2,57 @@ const { Stats } = require('../models');
 
 const getStats = async (req, res) => {
     try {
-        const userId = res.locals.payload.id;
-        const stats = await Stats.findOne({ where: { userId } });
-        res.json(stats || []);
+      const userId = res.locals.payload.id;
+      const stats = await Stats.findAll({ where: { userId } });
+      res.json(stats);
     } catch (error) {
-        console.error('Error fetching Stats:', error);
-        res.status(500).send('Server error');
+      console.error('Error fetching Stats:', error);
+      res.status(500).send('Server error');
     }
-};
+  };
+  
 
-const createStats = async (req, res) => {
+const createStats = async (req, res) => {  
+    const userId = res.locals.payload.id;
+    const statsDataArray = req.body;
+  
     try {
-        const userId = res.locals.payload.id;
-        const existingStats = await Stats.findOne({ where: { userId } });
-
+      for (const statsData of statsDataArray) {
+        const existingStats = await Stats.findOne({ where: { userId, diveTypeId: statsData.diveTypeId } });
+  
         if (existingStats) {
-            return res.status(400).send('Stats already exists');
+          return res.status(400).send('Stats already exists');
         }
-
-        const statsData = req.body;
-
-        const newStats = await Stats.create({ ...statsData, userId });
-        res.status(201).json(newStats);
+  
+        await Stats.create({ ...statsData, userId });
+      }
+  
+      res.status(201).send('Stats created successfully');
     } catch (error) {
-        console.error('Error creating Stats:', error);
-        res.status(500).send('Server error');
+      console.error('Error creating Stats:', error);
+      res.status(500).json({ error: 'Error creating Stats' });
     }
-};
+  };
 
-const updateStats = async (req, res) => {
+  const updateStats = async (req, res) => {
     try {
-        const userId = res.locals.payload.id;
-        const statsData = req.body;
-
-        const [updatedRows] = await Stats.update(statsData, { where: { userId } });
-        if (updatedRows > 0) {
-            const updatedStats = await Stats.findOne({ where: { userId } });
-            res.json(updatedStats);
-        } else {
-            res.status(404).send('Stats not found');
+      const userId = res.locals.payload.id;
+      const statsDataArray = req.body;
+  
+      for (const statsData of statsDataArray) {
+        const [updatedRows] = await Stats.update(statsData, { where: { userId, diveTypeId: statsData.diveTypeId } });
+        if (updatedRows === 0) {
+          return res.status(404).send('Stats not found');
         }
+      }
+  
+      const updatedStats = await Stats.findAll({ where: { userId } });
+      res.json(updatedStats);
     } catch (error) {
-        console.error('Error updating Stats:', error);
-        res.status(500).send('Server error');
+      console.error('Error updating Stats:', error);
+      res.status(500).send('Server error');
     }
-};
+  };
 
 module.exports = {
     getStats,
