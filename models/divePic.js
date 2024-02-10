7f2b6356b29cc188bd27ba042ec74ee90ff7dcd0
John Hansen <jhb.rgn@gmail.com>
2024-02-10 17:20:19 -0500

    Add sharePermission to DivePic

--- models/divePic.js
+++ models/divePic.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
 
     DivePic.init({
         filePath: DataTypes.STRING,
+        sharePermission: DataTypes.BOOLEAN,
     }, {
         sequelize,
         modelName: 'DivePic',
